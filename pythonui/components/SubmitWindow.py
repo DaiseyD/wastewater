fa6e611e6bbdae258736f76a2044897fc73e90fb
DoodleyJC <dijkstradesley@gmail.com>
2025-10-06 17:34:00 +0200

    added data functionalities of the submitrainframe and submitparameterframe

--- pythonui/components/SubmitWindow.py
+++ pythonui/components/SubmitWindow.py
@@ -1,9 +1,9 @@
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from components.popup import ErrorPopup
-
+from components.DataTarget import DataTarget
 # utility function for toggling a widget's visibility
-def toggleWidget(self, state, widget):
+def toggleWidget(state, widget):
     if(state==Qt.CheckState.Checked):
         widget.show()
     elif state==Qt.CheckState.Unchecked :
@@ -23,28 +23,69 @@ class SubmitWindow(QWidget):
         self.setupMain(mainvbox)
         
     def setupMain(self, layout):
-        self.setupTypeFrame(layout)
-        rainfallFrame = QFrame()
+        self.setupParamFrame(layout)
+        rainfallFrame = self.RainSubmitFrame(self.target['rainfallevents'])
         layout.addWidget(rainfallFrame)
 
-    def setupTypeFrame(self, layout):
-        typeFrame = QFrame()
-        layout.addWidget(typeFrame)
-        vbox = QVBoxLayout(typeFrame)
+    def setupParamFrame(self, layout):
+        parametersBaseFrame = QFrame()
+        layout.addWidget(parametersBaseFrame)
+        vbox = QVBoxLayout(parametersBaseFrame)
         vbox.addWidget(QLabel("typeParameters"))
         toggleBox = QCheckBox()
-        contentFrame = self.TypeFrame(self.target['parameters'])
+        contentFrame = self.ParamFrame(data=self.target['parameters'])
         toggleBox.checkStateChanged.connect(lambda state, w = contentFrame: toggleWidget(state, w))
         vbox.addWidget(toggleBox)
         vbox.addWidget(contentFrame)
         contentFrame.hide()
     
-    class TypeFrame(QFrame):
-        def init(self, data):
-            super().init()
+    class ParamFrame(QFrame):
+        def __init__(self, data):
+            super().__init__()
+            layout = QVBoxLayout(self)
             self.data = data
             for item in self.data:
-                QLabel(item)
+                layout.addWidget(self.FieldFrame(item, self.data[item]))
+    
+        class FieldFrame(QFrame):
+            def __init__(self, typeName, data):
+                super().__init__()
+                self.typeName = typeName
+                self.data = data
+                baseLayout = QVBoxLayout(self)
+                titleLayout = QHBoxLayout()
+                baseLayout.addLayout(titleLayout)
+                titleLayout.addWidget(QLabel(typeName),7)
+
+                toggleBox = QCheckBox()
+                frame = self.setupFieldFrames(baseLayout)
+                toggleBox.checkStateChanged.connect(lambda x, frame=frame : toggleWidget(x, frame))
+                frame.hide()
+                titleLayout.addWidget(toggleBox)
+                baseLayout.addWidget(frame)
+            
+            def setupFieldFrames(self, layout):
+                frame = QFrame()
+                layout.addWidget(frame)
+                frameLayout = QVBoxLayout(frame)    
+                for i in self.data:
+                    frameLayout.addWidget(QLabel(f"Strategy:{self.data[i]['strategy']}"))
+                    frameLayout.addWidget(QLabel(i))
+                return frame
+            
+    class RainSubmitFrame(QFrame):
+        def __init__(self, data):
+            super().__init__()
+            self.data = data
+            self.raindata = DataTarget().data['rainfallevents']
+            layout = QVBoxLayout(self)
+            for i in self.data:
+                raineventlayout = QHBoxLayout()
+                raineventlayout.addWidget(QLabel(f"{i}"))
+                rainname = list(filter((lambda item: item['id'] == i), self.raindata))[0]['name']
+                raineventlayout.addWidget(QLabel(rainname))
+                layout.addLayout(raineventlayout)
+
 
 
     
